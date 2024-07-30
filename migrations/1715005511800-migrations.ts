895fd9b234f55dc5bc284acd3d1f2556c93e938f
Artem <Artteem8@gmail.com>
2024-07-30 02:35:39 -0500

    Changed initial migration to have fake timestamp

--- migrations/1715005511800-migrations.ts
+++ migrations/1715005511800-migrations.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migrations1 implements MigrationInterface {
-    name = 'Migrations1'
+export class Migrations1715005511800 implements MigrationInterface {
+    name = 'Migrations1715005511800'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying NOT NULL, "password" character varying NOT NULL, "createdDate" TIMESTAMP NOT NULL DEFAULT now(), "updatedDate" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
